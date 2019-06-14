4531a23dd0fe4034b9ea044b604e40fafd9c53ef
Dmitriy Shikhalev <dmitriy.shikhalev@itglobal.com>
2019-06-14 18:42:07 +0500

    exclude contorllers from VM.get_disks()

--- pyvcloud/vcd/vm.py
+++ pyvcloud/vcd/vm.py
@@ -832,7 +832,7 @@ class VM(object):
     async def get_disks(self):
         disk_list = await self.client.get_resource(
             (await self.get_resource()).get('href') + '/virtualHardwareSection/disks')
-        return disk_list.Item
+        return [item for item in disk_list.Item if item[tag('rasd')('ResourceType')].text == '17']
 
 
     async def get_disk(self, disk_id):
